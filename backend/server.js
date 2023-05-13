adbeb32c4fbf7bc857d2baae18248136fe36f11c
ambooken004 <joeambooken1@gmail.com>
2023-05-13 23:44:34 +0530

    Initialized frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+    res.send("API is running though...");// created an API
+});
+
+
+
+
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server Running at PORT: ${PORT}`));// created a web server
