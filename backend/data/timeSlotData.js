deb7eb79af565500fdfc9203e53a04001aa5e403
ambooken004 <joeambooken1@gmail.com>
2023-05-19 14:18:42 +0530

    Modified Backend

---
+++ backend/data/timeSlotData.js
@@ -0,0 +1,79 @@
+const timeSlotData = [
+    {
+      id: "1",
+      time: "9:00AM - 9:30AM",
+      category: "Haircut",
+    },
+    {
+      id: "2",
+      time: "9:30AM - 10:00AM",
+      category: "Shaving",
+    },
+    {
+      id: "3",
+      time: "10:00AM - 10:30AM",
+      category: "Dyeing",
+    },
+    {
+        id: "4",
+        time: "10:30AM - 11:00AM",
+        category: "Hair Colouring",
+    },
+    {
+        id: "5",
+        time: "11:00AM - 11:30AM",
+        category: "Tattoing",
+    },
+    {
+        id: "6",
+        time: "11:30AM - 12:00PM",
+        category: "Tattoing",
+    },
+    {
+        id: "7",
+        time: "12:00PM - 12:30PM",
+        category: "Tattoing",
+    },
+    {
+        id: "8",
+        time: "12:30PM - 1:00PM",
+        category: "Tattoing",
+    },
+    {
+        id: "9",
+        time: "2:00PM - 2:30PM",
+        category: "Tattoing",
+    },
+    {
+        id: "10",
+        time: "2:30PM - 3:00PM",
+        category: "Tattoing",
+    },
+    {
+        id: "11",
+        time: "3:00PM - 3:30PM",
+        category: "Tattoing",
+    },
+    {
+        id: "12",
+        time: "3:30PM - 4:00PM",
+        category: "Tattoing",
+    },
+    {
+        id: "13",
+        time: "4:30PM - 5:00PM",
+        category: "Tattoing",
+    },
+    {
+        id: "14",
+        time: "5:00PM - 5:30PM",
+        category: "Tattoing",
+    },
+    {
+        id: "15",
+        time: "5:30PM - 6:00PM",
+        category: "Tattoing",
+    }
+  ];
+  
+  module.exports = timeSlotData;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const timeslotdatas = require('./data/timeSlotData');
 const app = express();
 const dotenv = require('dotenv').config();
 
@@ -7,9 +8,14 @@ app.get('/', (req, res) => {
     res.send("API is running though...");// created an API
 });
 
-app.get('/about', (req, res) => {
+/* app.get('/about', (req, res) => {
     res.send("This is about us page");// created an API
+}); */
+
+app.get('/api/timeslotdata', (req, res) => {
+    res.json(timeslotdatas);
 });
+
 
 
 
